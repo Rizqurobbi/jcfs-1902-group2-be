eed6e154a30df6f69d744ca580b7d9dfd648aff1
Zakki Rizqurobbi <zakkirizqurb@gmail.com>
2022-03-28 22:00:08 +0700

    FAR-01-zakki : Adding looping stocks,looping unit

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -11,27 +11,44 @@ module.exports = {
                 }
             }
             let { _sort, _order, status } = req.query
-            let getSql = `Select p.* from products p join status s on p.idstatus = s.idstatus WHERE s.idstatus=${status?`${db.escape(status)}`:2} ${filterQuery.length > 0 ?`AND ${filterQuery.join(" AND ")}`:''} ${_sort && _order ?`ORDER BY ${_sort} ${_order}`:''}`
+            let getSql = `Select p.* from products p join status s on p.idstatus = s.idstatus WHERE s.idstatus=${status ? `${db.escape(status)}` : 2} ${filterQuery.length > 0 ? `AND ${filterQuery.join(" AND ")}` : ''} ${_sort && _order ? `ORDER BY ${_sort} ${_order}` : ''}`
             let resultsProducts = await dbQuery(getSql)
             let resultsImages = await dbQuery(`Select * from images`)
+            let resultsStocks = await dbQuery(`select * from stocks`)
+            let resultsUnit = await dbQuery(`select * from stocks s join unit u on s.idunit=u.idunit`)
             console.log('Before', filterQuery)
             console.log('After', filterQuery.join(' AND '))
             console.log('Combined Script', getSql);
-            resultsProducts.forEach((value,index)=>{
+            resultsProducts.forEach((value, index) => {
                 value.images = []
                 value.stocks = []
-                resultsImages.forEach(val =>{
-                    if (value.idproduct==val.idproduct){
+                value.unit = []
+                resultsImages.forEach(val => {
+                    if (value.idproduct == val.idproduct) {
                         delete val.idproduct
                         value.images.push(val)
                     }
                 })
+                resultsStocks.forEach(val => {
+                    if (value.idproduct == val.idproduct) {
+                        delete val.idproduct
+                        value.stocks.push(val)
+                    }
+                })
+                resultsUnit.forEach(val => {
+                    delete val.idproduct
+                    delete val.idstock
+                    delete val.qty
+                    delete val.type
+                    value.unit.push(val)
+
+                })
             })
             res.status(200).send({
-                success:true,
-                message:'get product success ✅',
-                dataProducts:resultsProducts,
-                error:''
+                success: true,
+                message: 'get product success ✅',
+                dataProducts: resultsProducts,
+                error: ''
             })
         } catch (error) {
             console.log(error)
