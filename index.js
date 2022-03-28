fb7d3bf48405e84cc7d80f0995c444a925d570d0
Zakki Rizqurobbi <zakkirizqurb@gmail.com>
2022-03-28 20:51:38 +0700

    FAR-01-zakki : getProduct middleware

--- index.js
+++ index.js
@@ -5,16 +5,20 @@ const dotenv = require('dotenv');
 const bearerToken = require("express-bearer-token")
 dotenv.config();
 
-const PORT = process.env.PORT || 2000;
+const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 app.use(bearerToken()); // untuk mengambil data token dari req.header client
-
+const { db } = require('./supports/database')
 // DB Check Connection
-
-
+db.getConnection((err,connection)=>{
+    if(err){
+        console.log('Error mySql Connection :',err.message)
+    }
+    console.log(`Connected to MySql Server ✅ : ${connection.threadId}`)
+})
 // Routes API Setup
-
-
-app.listen(PORT, () => console.log("Your API RUNNING :", PORT));
+const {productsRoute} = require('./routers')
+app.use('/products',productsRoute)
+app.listen(PORT, () => console.log("Farmacia :", PORT));
