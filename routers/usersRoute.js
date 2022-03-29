cbcdf791f0ec884535cdacccd84cc660c529c2ef
fanotastic <fawwazputrou@gmail.com>
2022-03-29 12:35:44 +0700

    FAR-03-fawwaz: Verification for user done

--- routers/usersRoute.js
+++ routers/usersRoute.js
@@ -4,5 +4,6 @@ const { usersController } = require('../controllers')
 
 router.get('/', usersController.getData)
 router.post('/register', usersController.register);
+router.get('/verify', readToken, usersController.verify)
 
 module.exports = router
