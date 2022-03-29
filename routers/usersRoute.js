7ecafe426301a701ca6ad134513510ea5c8cee89
fanotastic <fawwazputrou@gmail.com>
2022-03-29 22:19:26 +0700

    FAR-04-fawwaz: login API and keep login success

--- routers/usersRoute.js
+++ routers/usersRoute.js
@@ -5,5 +5,7 @@ const { usersController } = require('../controllers')
 router.get('/', usersController.getData)
 router.post('/register', usersController.register);
 router.get('/verify', readToken, usersController.verify)
+router.post('/login', usersController.login)
+router.get('/keeplogin', readToken, usersController.keepLogin);
 
 module.exports = router
